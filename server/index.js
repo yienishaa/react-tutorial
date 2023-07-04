0a1d11d3ae1aaa30d2fa073317b3aedbdc134bd2
Yienisha Abeyratne <97560226+yienishaa@users.noreply.github.com>
2023-07-03 21:41:13 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 //connecting the DB
-const CONNECTION_URL = 'mongodb+srv://admin:CoWRp32Q6RxRbbp1@cluster0.nxtldzk.mongodb.net/';
+const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster0.nxtldzk.mongodb.net/';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
